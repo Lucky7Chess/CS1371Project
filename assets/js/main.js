ddac00e6307364a1e796f5df5d278b7f3f0d48f8
Kenny Shaw <lucky7chess@gmail.om>
2017-12-02 19:54:39 -0500

    Got the interface looking nice, now for the javascript

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,9 +1,10 @@
-/*
-	Photon by HTML5 UP
-	html5up.net | @ajlkn
-	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
-*/
 
+
+
+
+
+
+//Below is mostly scrolly setup code for the fun animation :)
 (function($) {
 
 	skel.breakpoints({
